7cdd22a79c4be69d98d355584f041b910df4b2b5
charLLLLLie <mega.joker00@gmail.com>
2021-05-25 19:45:49 +0300

    a

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,13 +1,13 @@
 import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
-    // CART_SAVE_SHIPPING_ADDRESS,
-    // CART_SAVE_PAYMENT_METHOD,
+    CART_SAVE_SHIPPING_ADDRESS,
+    CART_SAVE_PAYMENT_METHOD,
     // CART_CLEAR_ITEMS,
   } from '../constants/cartConstants'
   
   export const cartReducer = (
-    state = { cartItems: [] },
+    state = { cartItems: [], shippingAddress:{}},
     action
   ) => {
     switch (action.type) {
@@ -35,16 +35,16 @@ import {
           ...state,
           cartItems: state.cartItems.filter((x) => x.product !== action.payload),
         }
-    //   case CART_SAVE_SHIPPING_ADDRESS:
-    //     return {
-    //       ...state,
-    //       shippingAddress: action.payload,
-    //     }  
-    //   case CART_SAVE_PAYMENT_METHOD:
-    //     return {
-    //       ...state,
-    //       paymentMethod: action.payload,
-    //     }
+      case CART_SAVE_SHIPPING_ADDRESS:
+        return {
+          ...state,
+          shippingAddress: action.payload,
+        }  
+      case CART_SAVE_PAYMENT_METHOD:
+        return {
+          ...state,
+          paymentMethod: action.payload,
+        }
     //   case CART_CLEAR_ITEMS:
     //     return {
     //       ...state,
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,17 +1,25 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux'
 import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import {
+  composeWithDevTools
+} from 'redux-devtools-extension'
 import {
   productListReducer,
   productDetailsReducer,
   productDeleteReducer,
-  productCreateReducer, 
+  productCreateReducer,
   productUpdateReducer,
   productReviewCreateReducer,
   productTopRatedReducer,
 } from './reducers/productReducers'
 
- import { cartReducer } from './reducers/cartReducers'
+import {
+  cartReducer
+} from './reducers/cartReducers'
 import {
   userLoginReducer,
   userRegisterReducer,
@@ -21,57 +29,59 @@ import {
   userDeleteReducer,
   userUpdateReducer,
 } from './reducers/userReducers'
-// import {
-//   orderCreateReducer,
-//   orderDetailsReducer,
-//   orderPayReducer,
-//   orderDeliverReducer,
-//   orderListMyReducer,
-//   orderListReducer,
-// } from './reducers/orderReducers'
+import {
+  orderCreateReducer,
+  orderDetailsReducer,
+  orderPayReducer,
+  //   orderDeliverReducer,
+  orderListMyReducer,
+  //orderListReducer,
+} from './reducers/orderReducers'
 
 const reducer = combineReducers({
-   productList: productListReducer,
-   productDetails: productDetailsReducer,
-//   productDelete: productDeleteReducer,
-//   productCreate: productCreateReducer,
-//   productUpdate: productUpdateReducer,
-//   productReviewCreate: productReviewCreateReducer,
-//   productTopRated: productTopRatedReducer,
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  //   productDelete: productDeleteReducer,
+  //   productCreate: productCreateReducer,
+  //   productUpdate: productUpdateReducer,
+  //   productReviewCreate: productReviewCreateReducer,
+  //   productTopRated: productTopRatedReducer,
   cart: cartReducer,
- userLogin: userLoginReducer,
- userRegister: userRegisterReducer,
- userDetails: userDetailsReducer,
- userUpdateProfile: userUpdateProfileReducer,
-//   userList: userListReducer,
-//   userDelete: userDeleteReducer,
-//   userUpdate: userUpdateReducer,
-//   orderCreate: orderCreateReducer,
-//   orderDetails: orderDetailsReducer,
-//   orderPay: orderPayReducer,
-//   orderDeliver: orderDeliverReducer,
-//   orderListMy: orderListMyReducer,
-//   orderList: orderListReducer,
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
+  userDelete: userDeleteReducer,
+  userUpdate: userUpdateReducer,
+  orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
+  orderPay: orderPayReducer,
+  //   orderDeliver: orderDeliverReducer,
+  orderListMy: orderListMyReducer,
+  //   orderList: orderListReducer,
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems')
-  ? JSON.parse(localStorage.getItem('cartItems'))
-  : []
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+  JSON.parse(localStorage.getItem('cartItems')) :
+  []
 
-const userInfoFromStorage = localStorage.getItem('userInfo')
-  ? JSON.parse(localStorage.getItem('userInfo'))
-  : null
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+  JSON.parse(localStorage.getItem('userInfo')) :
+  null
 
-// const shippingAddressFromStorage = localStorage.getItem('shippingAddress')
-//   ? JSON.parse(localStorage.getItem('shippingAddress'))
-//   : {}
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ?
+  JSON.parse(localStorage.getItem('shippingAddress')) :
+  {}
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
-    //shippingAddress: shippingAddressFromStorage,
+    shippingAddress: shippingAddressFromStorage,
   },
-  userLogin: { userInfo: userInfoFromStorage },
+  userLogin: {
+    userInfo: userInfoFromStorage
+  },
 }
 
 const middleware = [thunk]
